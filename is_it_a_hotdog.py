c5b9f5fe624e244328969e9640109657bc8199b3
awoinoski <74275159+awoinoski@users.noreply.github.com>
2023-11-02 17:12:49 -0700

    Starting to actually write some code. So far we have set the paths to the where the data is and next I will be working on opening the image files.

--- is_it_a_hotdog.py
+++ is_it_a_hotdog.py
@@ -1,5 +1,18 @@
 import torch
 from pathlib import Path
+import numpy as np
+import glob
 
-#code and stuff
+# Set paths to access training data and test data
+data_dir = Path.cwd() / "archive"
+train_dir = data_dir / "train"
+test_dir = data_dir / "test"
 
+# 
+image_sets = ["hot_dog","not_hot_dog"]
+
+for sets in image_sets:
+    image_paths = glob.glob(str(train_dir / "hot_dog\*"))
+    #images = np.load(image_paths[0])
+    
+    print(image_paths[0])
